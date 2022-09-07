c78596cfe22cad27c8a04d29526eecddc5e3bc65
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-07 14:52:54 +0200

    test update

--- schema/updates/2022_09_07_Test_permission.sql
+++ schema/updates/2022_09_07_Test_permission.sql
@@ -1,4 +1,4 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_09_07_Test_ermission.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_09_07_Test_permission.sql', 'ONGOING');
 
 INSERT into Permission (permissionId, type, description) VALUES (10001, "test", "Test permission");
 
