38b94b5453e33c7e12d29d9d803bb15bcafd836a
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-07 14:56:07 +0200

    test success

--- schema/updates/2022_09_07_Test_permission.sql
+++ schema/updates/2022_09_07_Test_permission.sql
@@ -2,4 +2,4 @@ INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_09_07_Test_per
 
 INSERT into Permission (permissionId, type, description) VALUES (10001, "test", "Test permission");
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2022_09_07_Test_ermission.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2022_09_07_Test_permission.sql';
