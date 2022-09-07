6d3f0d439350fd95bcd27400d6c11d4e83469566
Stuart Fisher <stuart.fisher@esrf.fr>
2022-09-07 15:01:34 +0200

    tidy

--- schema/updates/2022_09_07_Test_permission.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_09_07_Test_permission.sql', 'ONGOING');
-
-INSERT into Permission (permissionId, type, description) VALUES (10001, "test", "Test permission");
-
-UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2022_09_07_Test_permission.sql';
