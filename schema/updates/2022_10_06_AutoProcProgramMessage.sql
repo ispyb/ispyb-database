cb3519028e3ff774a3dc45a8fb321fef25eb06c3
Stuart Fisher <stuart.fisher@esrf.fr>
2022-10-06 16:46:01 +0200

    add AutoProcProgramMessage

--- schema/updates/2022_10_06_AutoProcProgramMessage.sql
+++ schema/updates/2022_10_06_AutoProcProgramMessage.sql
@@ -1,12 +1,12 @@
 INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_10_06_AutoProcProgramMessage.sql', 'ONGOING');
 
-CREATE TABLE AutoProcProgramMessage (
-  autoProcProgramMessageId int(10) unsigned NOT NULL auto_increment; PRIMARY KEY,
-  autoProcProgramId int unsigned,
-  recordTimeStamp timestamp DEFAULT current_timestamp,
-  severity enum ('ERROR', 'WARNING', 'INFO'),
-  message varchar(200),
-  description text,
+CREATE TABLE `AutoProcProgramMessage` (
+  `autoProcProgramMessageId` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+  `autoProcProgramId` int unsigned,
+  `recordTimeStamp` timestamp DEFAULT current_timestamp,
+  `severity` enum ('ERROR', 'WARNING', 'INFO'),
+  `message` varchar(200),
+  `description` text,
   CONSTRAINT AutoProcProgramMessage_fk1 FOREIGN KEY (autoProcProgramId) REFERENCES AutoProcProgram (autoProcProgramId)
 );
 
